e2eb4c0be20f1c3e81fb13f4c0086bed04673c87
purprose121 <yksnydro@gmail.com>
2024-05-06 19:46:13 +0300

    Work with logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     @IBOutlet var redTrafficLight: UIView!
     @IBOutlet var yellowTrafficLight: UIView!
     @IBOutlet var greenTrafficLight: UIView!
@@ -15,11 +15,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         redTrafficLight.layer.cornerRadius = 64
         yellowTrafficLight.layer.cornerRadius = 64
         greenTrafficLight.layer.cornerRadius = 64
         startTrafficLightButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startTrafficLight() {
+        let maxBrightness: CGFloat = 1
+        
+        switch maxBrightness {
+        case redTrafficLight.alpha:
+            yellowTrafficLight.alpha = 1
+            redTrafficLight.alpha = 0.3
+        case yellowTrafficLight.alpha:
+            greenTrafficLight.alpha = 1
+            yellowTrafficLight.alpha = 0.3
+        case greenTrafficLight.alpha:
+            redTrafficLight.alpha = 1
+            greenTrafficLight.alpha = 0.3
+        default:
+            redTrafficLight.alpha = 1
+        }
+        
+        changeNameButton()
+    }
+    
+    private func changeNameButton() {
+        if self.startTrafficLightButton.titleLabel?.text == "START" {
+            self.startTrafficLightButton.setTitle("NEXT",for: .normal)
+        }
+    }
 }
