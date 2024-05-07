64e00eafb6ee7f1eb27f724a90e4a51cbc6378a2
purprose121 <yksnydro@gmail.com>
2024-05-07 17:57:25 +0300

    Refactor UI

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -13,38 +13,39 @@ final class ViewController: UIViewController {
     @IBOutlet var greenTrafficLight: UIView!
     @IBOutlet var startTrafficLightButton: UIButton!
     
+    let maxBrightness: CGFloat = 1
+    let minBrightness: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redTrafficLight.layer.cornerRadius = 64
-        yellowTrafficLight.layer.cornerRadius = 64
-        greenTrafficLight.layer.cornerRadius = 64
+        redTrafficLight.layer.cornerRadius = redTrafficLight.frame.size.height / 2
+        yellowTrafficLight.layer.cornerRadius = yellowTrafficLight.frame.size.height / 2
+        greenTrafficLight.layer.cornerRadius = greenTrafficLight.frame.size.height / 2
         startTrafficLightButton.layer.cornerRadius = 10
     }
 
     @IBAction func startTrafficLight() {
-        let maxBrightness: CGFloat = 1
-        
         switch maxBrightness {
         case redTrafficLight.alpha:
-            yellowTrafficLight.alpha = 1
-            redTrafficLight.alpha = 0.3
+            yellowTrafficLight.alpha = maxBrightness
+            redTrafficLight.alpha = minBrightness
         case yellowTrafficLight.alpha:
-            greenTrafficLight.alpha = 1
-            yellowTrafficLight.alpha = 0.3
+            greenTrafficLight.alpha = maxBrightness
+            yellowTrafficLight.alpha = minBrightness
         case greenTrafficLight.alpha:
-            redTrafficLight.alpha = 1
-            greenTrafficLight.alpha = 0.3
+            redTrafficLight.alpha = maxBrightness
+            greenTrafficLight.alpha = minBrightness
         default:
-            redTrafficLight.alpha = 1
+            redTrafficLight.alpha = maxBrightness
         }
         
         changeNameButton()
     }
     
     private func changeNameButton() {
-        if self.startTrafficLightButton.titleLabel?.text == "START" {
-            self.startTrafficLightButton.setTitle("NEXT",for: .normal)
+        if startTrafficLightButton.titleLabel?.text == "START" {
+            startTrafficLightButton.setTitle("NEXT",for: .normal)
         }
     }
 }
